462b1a29b716ac09bc0db733c4360d93015f74f3
Braden Lamb <chewyiscrunchy@gmail.com>
2020-11-27 22:45:45 -0500

    Bot: Better comments

--- src/Bot.ts
+++ src/Bot.ts
@@ -17,7 +17,7 @@ export type BotConfig = {
  * 
  * @param {BotConfig} config Bot configuration
  * 
- * @returns {BotConfig} Bot configuration
+ * @returns {BotConfig} Bot configuration.
  */
 export function validateBotConfig (config?: BotConfig): BotConfig {
     if (typeof config !== 'object' || config === null) throw new TypeError('Bot configuration must be an object')
@@ -27,6 +27,17 @@ export function validateBotConfig (config?: BotConfig): BotConfig {
     if (!Array.isArray(config.whitelist) && config.whitelist) throw new TypeError('Whitelist must be an array of strings')
     if (!Array.isArray(config.blacklist) && config.blacklist) throw new TypeError('Blacklist must be an array of strings')
     return config
+}
+
+/**
+ * Async setTimeout helper.
+ * 
+ * @param ms Timeout in milliseconds
+ * 
+ * @returns {Promise<void>} Fullfilled when the timeout is completed.
+ */
+export function wait (ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 /**
@@ -51,7 +62,7 @@ export class Bot {
      * 
      * @public
      * 
-     * @returns {Promise<void>} Fullfilled when the Bot has logged in
+     * @returns {Promise<void>} Fullfilled when the Bot has logged in.
      */
     public async login (): Promise<void> {
         this.client.state.generateDevice(this.config.username)
@@ -61,20 +72,51 @@ export class Bot {
         await this.processFeed()
     }
 
-    private async processFeed (): Promise<void> {
-        const feed = this.client.feed.timeline('pull_to_refresh')
-        const items = await feed.items()
-        await this.processMediaItems(items)
-        await new Promise(resolve => setTimeout(resolve, 60000))
+    /**
+     * Get posts from feed and process them.
+     * 
+     * @private
+     * 
+     * @returns {Promise<void>} Fullfilled when all of the posts are processed.
+     */
+    private async processFeed (): Promise<void> {   
+        try {
+            const items = await this.client.feed.timeline('pull_to_refresh').items()
+            await this.processMediaItems(items)
+            await wait(60 * 1000 * (items.length === 0 ? 5 : 1))
+        } catch (error) {
+            console.error(error)
+            await wait(60 * 60 * 1000)
+        }
+
         this.processFeed()
     }
 
+
+    /**
+     * Process an array of media items.
+     * 
+     * @private
+     * 
+     * @param {TimelineFeedResponseMedia_or_ad[]} items Array of media items
+     * 
+     * @returns {Promise<void>} Fullfilled when the items are processed.
+     */
     private async processMediaItems (items: TimelineFeedResponseMedia_or_ad[]): Promise<void> {
         for (const item of items) {
             await this.processMediaItem(item)
         }
     }
 
+    /**
+     * Process a media item.
+     * 
+     * @private
+     * 
+     * @param item Media item
+     * 
+     * @returns {Promise<void>} Fullfilled when the media item has been processed.
+     */
     private async processMediaItem (item: TimelineFeedResponseMedia_or_ad): Promise<void> {
         if (item.has_liked) return
 
@@ -83,12 +125,24 @@ export class Bot {
         const likeTimeout = (10 + (Math.random() * 6)) * 1000
         const commentTimeout = (60 + (Math.random() * 30)) * 1000
 
-        await this.client.media.like({ d: 1, mediaId, moduleInfo: { module_name: 'feed_timeline' }})
-        await new Promise(resolve => setTimeout(resolve, likeTimeout))
-        await this.client.media.comment({ mediaId, text })
-        await new Promise(resolve => setTimeout(resolve, commentTimeout))
+        try {
+            await this.client.media.like({ d: 1, mediaId, moduleInfo: { module_name: 'feed_timeline' }})
+            await wait(likeTimeout)
+            await this.client.media.comment({ mediaId, text })
+            await wait(commentTimeout)
+        } catch (error) {
+            console.error(error)
+            await wait(60 * 60 * 1000)
+        }
     }
 
+    /**
+     * Returns a random comment from the current config.
+     * 
+     * @private
+     * 
+     * @returns {string} Comment text.
+     */
     private randomComment (): string {
         return this.config.comments[Math.floor(Math.random() * this.config.comments.length)]
     }
